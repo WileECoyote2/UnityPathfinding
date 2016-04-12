b47361f219141f1c7aaa19085aa51014b89793ad
JakeyCouper <WileECoyote2@users.noreply.github.com>
2016-04-12 09:50:20 -0700

    fixed the Graph.cs syntax

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -7,46 +7,47 @@ public class Graph {
 	public int cols = 0;
 	public Node[] nodes;
 
-
 	public Graph(int[,] grid){
 		rows = grid.GetLength (0);
 		cols = grid.GetLength (1);
 
 		nodes = new Node[grid.Length];
-		for (ShaderVariantCollection i = 0; i < nodes.Length; i++) {
+		for (var i = 0; i < nodes.Length; i++) {
 			var node = new Node();
 			node.label = i.ToString();
-			nodes[i} = node;
+			nodes[i] = node;
 		}
 
-			      for(var r = 0; r < rows; r++) {
-					for(var c = 0; c < cols; c++){
-					var node = nodes [cols * r + cw];
+		for (var r = 0; r < rows; r++) {
+			for(var c = 0; c < cols; c++){
+				var node = nodes[cols*r + c];
 
-					if(grid[r,c] == 1){
-						continue;
-					}
+				if(grid[r,c] == 1){
+					continue;
+				}
 
-					//Up
-					if(r > 0){
-						Node.adjacent.add(nodes[cols*(r-1)+c]);
-					}
+				// Up
+				if(r > 0){
+					node.adjecent.Add(nodes[cols*(r-1)+c]);
+				}
 
-					//Right
-					if(c < cols-1){
-						Node.Adjacent.Add(nodes[cols*r+c+1]);
-					}
+				// Right
+				if(c < cols-1){
+					node.adjecent.Add(nodes[cols*r+c+1]);
+				}
 
-					//Down
-					if(r<rows-1){
-						Node.adjacent.Add(nodes[cols*(r+1)+c]);
-					}
+				// Down
+				if(r < rows-1){
+					node.adjecent.Add(nodes[cols*(r+1)+c]);
+				}
 
-					//Left
-					if(c > 0){
-						Node.adjacent.add(nodes[cols*r+c-1]);
-					} 
-			} 
-	     }
+				// Left
+				if(c > 0){
+					node.adjecent.Add(nodes[cols*r+c-1]);
+				}
+
+			}
+		}
 	}
 
+}
