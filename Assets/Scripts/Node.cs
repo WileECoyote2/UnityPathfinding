a5ad4ff21171f3f783cebe02d5c4e1fddf3e2aea
JakeyCouper <WileECoyote2@users.noreply.github.com>
2016-04-12 09:57:48 -0700

    fixed a missing definition in Node.cs

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 
 public class Node {
 
-	public List <Node> adjacent = new List<Node>();
+	public List<Node> adjecent = new List<Node>();
 	public Node previous = null;
 	public string label = "";
 
 	public void Clear(){
 		previous = null;
 	}
+
 }
